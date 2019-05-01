607a1f3729411ad66d7c28b22b407e7ba1a8bcbc
moiseshilario <moiseshilariu@gmail.com>
2019-05-01 17:29:04 -0300

    Add Available controller/logic/html

--- src/routes.js
+++ src/routes.js
@@ -12,6 +12,7 @@ const SessionController = require('./app/controllers/SessionController')
 const DashboardController = require('./app/controllers/DashboardController')
 const FileController = require('./app/controllers/FileController')
 const AppointmentController = require('./app/controllers/AppointmentController')
+const AvailableController = require('./app/controllers/AvailableController')
 
 routes.use((req, res, next) => {
   res.locals.flashSuccess = req.flash('success')
@@ -35,5 +36,6 @@ routes.get('/app/logout', SessionController.destroy)
 routes.get('/app/dashboard', DashboardController.index)
 
 routes.get('/app/appointments/new/:provider', AppointmentController.create)
+routes.get('/app/available/:provider', AvailableController.index)
 
 module.exports = routes
