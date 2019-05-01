33d2089c9cc4c4c63102e900492ce27a420eb51e
moiseshilario <moiseshilariu@gmail.com>
2019-05-01 01:37:38 -0300

    Add authentication

--- src/app/models/User.js
+++ src/app/models/User.js
@@ -22,5 +22,9 @@ module.exports = (sequelize, DataType) => {
     }
   )
 
+  User.prototype.checkPassword = function (password) {
+    return bcrypt.compare(password, this.password_hash)
+  }
+
   return User
 }
